666c58ce9904781d305d5676d1512c4dcd8f9186
Jorgen G. Nielsen <jgn@dbc.dk>
2013-01-21 17:10:38 +0100

    updated error handling

--- localisation-agencies.tpl.php
+++ localisation-agencies.tpl.php
@@ -1,10 +1,18 @@
 
 <div id="<?php $agencies['branchId'] ?>" class="localisation-agency">
-  <p class="agencyName"><?php print $agencies['agencyName']; ?></p>
+<?php if ( $agencies['branchName'] != $agencies['agencyName']) { ?>
   <p class="branchName"><?php print $agencies['branchName']; ?></p>
+<?php } ?>
+  <p class="agencyName"><?php print $agencies['agencyName']; ?></p>
   <p class="postalAddress"><?php print $agencies['postalAddress']; ?></p>
   <p class="postalCode"><?php print $agencies['postalCode']; ?></p>
   <p class="city"><?php print $agencies['city']; ?></p>
   <p class="holdings visuallyhidden"></p>
   <p class="lookupUrl"><?php echo drupal_render($agencies['lookupUrl']); ?></p>
+<?php if ( !empty($agencies['note']) ) { ?>
+  <p class="note"><?php print $agencies['note']; ?></p>
+<?php } ?>
+<?php if ( isset($agencies['error']) ) { ?>
+  <p class="note"><?php print $agencies['error']; ?></p>
+<?php } ?>
 </div>

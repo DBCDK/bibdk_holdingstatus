6794099626f45d9b8eae1cba52a92297f379eaec
Søren Vibjerg <vibjerg@gmail.com>
2013-01-24 14:20:13 +0100

    Holding status implemented with ajax

--- holding-status.tpl.php
+++ holding-status.tpl.php
@@ -3,7 +3,7 @@
  * This theme prints out the holding status
  */
 ?>
-<div class='<?php print $classes; ?>'>
+<div class='holding-status'>
     <span class="status <?php print $status; ?>"></span>
   <?php print $content; ?>
 </div>

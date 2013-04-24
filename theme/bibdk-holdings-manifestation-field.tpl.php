b2d336e6076ec2195cac1d8567a9f9f03b9bca7e
JrgenGNielsen <jgn@dbc.dk>
2013-04-24 16:45:17 +0200

    add popover class and click function to field

--- theme/bibdk-holdings-manifestation-field.tpl.php
+++ theme/bibdk-holdings-manifestation-field.tpl.php
@@ -2,7 +2,7 @@
     <a href="#<?php print $path; ?>" class="linkme-button load-holdings text-small text-lightgrey">
         <span class="icon icon-left icon-lightgrey-link">&nbsp;</span><?php print $link_text; ?>
     </a>
-    <div class='element-wrapper visuallyhidden linkme-wrapper'>
+    <div class='popover element-wrapper visuallyhidden linkme-wrapper'>
         <div class=''>
           <?php print drupal_render($favourites) ?><span class="close icon icon-left icon-red-x">&nbsp;</span>
         </div>
